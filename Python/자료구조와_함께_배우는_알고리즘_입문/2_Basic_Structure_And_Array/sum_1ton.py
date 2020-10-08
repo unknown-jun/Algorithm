510e0166deada450f29d99fe104c55e33787a9fb
unknown-jun <3461419@gmail.com>
2020-10-08 19:55:55 +0900

    Code pratice

---
+++ Python/자료구조와_함께_배우는_알고리즘_입문/2_Basic_Structure_And_Array/sum_1ton.py
@@ -0,0 +1,12 @@
+# Calculate total value of three integer from 1 to n
+
+def sum_1ton(n):
+
+    s = 0
+    while n > 50:
+        s += n
+        n -= 1
+    return s
+
+x = int(input('Enter x value: '))
+print(f'sum of integer from 1 to {x} is {sum_1ton(x)}')
