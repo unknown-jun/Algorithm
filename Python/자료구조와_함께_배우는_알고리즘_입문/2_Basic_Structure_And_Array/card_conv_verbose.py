22fab3d3c581aefc3d76e77da208f58af2351963
unknown-jun <3461419@gmail.com>
2020-10-08 16:43:52 +0900

    Code pratice

---
+++ Python/자료구조와_함께_배우는_알고리즘_입문/2_Basic_Structure_And_Array/card_conv_verbose.py
@@ -0,0 +1,30 @@
+# # print out base notation among 2~36 converting with decimal number
+
+def card_conv(x: int, r: int) -> str:
+    """Return string representative number which convert integer 'x' to 'r' base notation"""
+
+    d = ''    ## The string after converting
+    dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    n = len(str(x))
+
+    print(f'{r:2} | {x:{n}d}')
+    
+if __name__ == '__main__':
+    print("Converts a decimal number to an 'n' base notation.") 
+
+    while True:
+        while True:    # Take the value which is a non-nagative integer
+            no = int(input('Enter a non-negative integer for the conversion value: '))
+            if no > 0:
+                break
+
+        while True:    # Take the integer of 2 ~ 36 base notation
+            cd = int(input('what kind of base notation do you want to convert?: '))
+            if 2 <= cd <= 36:
+                break
+
+        print(f'Here is the {card_conv(no, cd)} for {cd} base notation.')
+
+        retry = input("Do you want to convert one more time? (Y/N): ")
+        if retry in {'N', 'n'}:
+            break
