1b728a8834f271c4938049a55c195da1127b0ddc
Lee Junhyeok <70187490+unknown-jun@users.noreply.github.com>
2022-08-17 22:16:06 +0900

    Add files via upload

--- Python/자료구조와_함께_배우는_알고리즘_입문/4_Stack_And_Queue/fixed_stack.py
+++ Python/자료구조와_함께_배우는_알고리즘_입문/4_Stack_And_Queue/fixed_stack.py
@@ -38,7 +38,7 @@ class FixedStack:
         self.stk[self.ptr] = value
         self.ptr += 1
         
-    def pop(self, value: Any) -> None:
+    def pop(self) -> None:
         if self.is_empty():          # 스택이 비어 있는 경우
             raise FixedStack.Empty   # 예외 처리 발생
         self.ptr -= 1
@@ -53,3 +53,29 @@ class FixedStack:
     def clear(self) -> None:
         '''스택을 비움(모든 데이터를 삭제)'''
         self.ptr = 0
+        
+    def find(self, value: Any) -> Any:
+        '''스택에서 value를 찾아 인덱스를 반환(없으면 -1을 반환)'''
+        for i in range(self.ptr-1, -1, -1):  # 꼭대기 쪽부터 선형 검색
+            if self.stk[i]==value:
+                return i   # 검색 성공
+        return -1          # 검색 실패
+    
+    def count(self, value: Any) -> int:
+        '''스택에 있는 value의 개수를 반환'''
+        c = 0
+        for i in range(self.ptr):     # 바닥 쪽부터 선형 검색
+            if self.stk[i] == value:  # 검색 성공
+                c += 1
+        return c
+    
+    def __contains__(self, value: Any) -> bool:
+        '''스택에 value가 있는지 판단'''
+        return self.count(value)
+    
+    def dump(self) -> None:
+        '''덤프(스택 안의 모든 데이터를 바닥부터 꼭대기 순으로 출력)'''
+        if self.is_empty():       # 스택이 비어있음
+            print('스택이 비어 있습니다.')
+        else:
+            print(self.stk[:self.ptr])
